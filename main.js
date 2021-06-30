65e11aee6f4e937a2fe7e91ee1520621f8fd8319
xingchenhe <946834832@qq.com>
2021-06-30 18:55:48 +0800

    crash日志

--- main.js
+++ main.js
@@ -181,6 +181,14 @@ app.on("child-process-gone",(event)=>{
   // }event.preventDefault()
   event.preventDefault()
 })
+console.log(crashReporter.getLastCrashReport(),'report')
+crashReporter.start({
+  productName: 'im-sdk',
+  companyName: 'tencent',
+  submitURL: 'http://127.0.0.1:5000/crash',
+  uploadToServer: true
+})
+
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
