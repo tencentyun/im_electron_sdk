5ee053ae1e7cda286462639f560ce4c54c645e24
Jingfeng <joyxian@tencent.com>
2021-07-01 18:39:27 +0800

    test

--- im_electron_sdk/src/tim.ts
+++ im_electron_sdk/src/tim.ts
@@ -8,7 +8,7 @@ import GroupManager from "./manager/groupManager";
 import TimbaseManager from "./manager/timbaseManager";
 
 class TIM {
-    private _sdkconfig: sdkconfig = {
+    private _sdkconfig: sdkconfig =  {
         sdkappid: 0,
         consoleTag: CONSOLETAG,
         Imsdklib: Imsdklib,
