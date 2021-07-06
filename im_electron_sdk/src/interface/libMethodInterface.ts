208b0a755b1b9c84f32741532a1bbc5ca6a05815
hexingcheng <946834832@qq.com>
2021-07-06 12:21:25 +0800

    '修改构建方式'

--- im_electron_sdk/src/interface/libMethodInterface.ts
+++ im_electron_sdk/src/interface/libMethodInterface.ts
@@ -552,7 +552,7 @@ interface TIMSetLogCallbackLibFun {
     (callback: Buffer, userData?: Buffer): void;
 }
 interface TIMSetConfigLibFun {
-    (json_config: Buffer, callback: Buffer,user_data: Buffer): number;
+    (json_config: Buffer, callback: Buffer, user_data: Buffer): number;
 }
 // ==========Interface For friendship End===========
 
@@ -569,8 +569,8 @@ interface libMethods {
     TIMSetNetworkStatusListenerCallback: TIMSetNetworkStatusListenerCallbackFun;
     TIMSetKickedOfflineCallback: TIMSetKickedOfflineCallbackFun;
     TIMSetUserSigExpiredCallback: TIMSetUserSigExpiredCallbackFun;
-    TIMSetLogCallback:TIMSetLogCallbackLibFun,
-    TIMSetConfig: TIMSetConfigLibFun
+    TIMSetLogCallback: TIMSetLogCallbackLibFun;
+    TIMSetConfig: TIMSetConfigLibFun;
     // timbase end
 
     // conversation start
