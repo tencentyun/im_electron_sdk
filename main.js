ae2e71c53e9292b69616adab7c8ed3f5366a4d14
xingchenhe(何星辰) <xingchenhe@tencent.com>
2021-08-12 14:58:02 +0800

    去掉开机自启动

--- main.js
+++ main.js
@@ -85,10 +85,10 @@ app.on('window-all-closed', function () {
 
 // 设置开机自启动
 
-app.setLoginItemSettings({
-  openAtLogin: true,
-  path: process.execPath
-})
+// app.setLoginItemSettings({
+//   openAtLogin: true,
+//   path: process.execPath
+// })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
